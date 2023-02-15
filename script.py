8d1091bedd958e31cbac0886608f985b635c3c27
Abrahamv19 <hugoabrahamv19@gmail.com>
2023-02-15 14:10:09 -0500

    commit inicial

---
+++ script.py
@@ -0,0 +1,11 @@
+# Cuando doy git init script.py cambia a color verde y aparece un U al frente del archivo
+# (ls -la) = a usar (ll),  me sirve pra ver los archivos ocultos de git, los accesos o permisos que tiene la carpeta donde escribi git init
+# pwd en la terminal me muestra donde estoy parado
+# git branch -m main para cambiar el nombre de master a main
+# git add script.py Agrega el el codigo al estado staging (recuerda que hay 3 estados = working directory, ejecuto git add y paso as segundo estado, staging area, ejecuto git commit y paso a tercer estado, repository )
+
+print('Hola mundo')
+
+
+
+
